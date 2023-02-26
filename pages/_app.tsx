beb9f847119bd04e8164826582fb99f42d3e58a4
shermaine sng <shersmallsng@gmail.com>
2023-02-26 23:33:01 +0800

    set up scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.css";
+import '../styles/bootstrap-custom.scss'; //override the bootstrap classes with my own
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+// Global Styles
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap")
+      : null;
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
+
+export default App;
